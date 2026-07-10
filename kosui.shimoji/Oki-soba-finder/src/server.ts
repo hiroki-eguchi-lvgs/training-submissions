bb95f6deaf18b621237ba05b2429fa1b84d9d775
kosui-shimoji-lvgs <kosui.shimoji@leverages.jp>
2026-07-10 11:45:35 +0900

    アプリをコンテナ化

--- kosui.shimoji/Oki-soba-finder/src/server.ts
+++ kosui.shimoji/Oki-soba-finder/src/server.ts
@@ -9,7 +9,7 @@ const start = async () => {
   try {
     const [rows] = await pool.query("SELECT 1 AS ok");
     app.log.info({ rows }, "DB接続OK");
-    await app.listen({ port });
+    await app.listen({ port, host: "0.0.0.0" });
   } catch (err) {
     app.log.error(err);
     process.exit(1);
