35113c9584e23b17cfc02b7df612a3b4bd5c6317
hiroki-eguchi-lvgs <hiroki.eguchi@leverages.jp>
2026-08-20 13:53:47 +0900

    6.7. serviceの中身を、実際のDBから記事一覧を取得する実装に置き換える

--- eguchi-hiroki/blog-backend/src/articles/articles.module.ts
+++ eguchi-hiroki/blog-backend/src/articles/articles.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { ArticlesController } from './articles.controller';
 import { ArticlesService } from './articles.service';
 
 @Module({
+  imports: [ConfigModule],
   controllers: [ArticlesController],
   providers: [ArticlesService]
 })
