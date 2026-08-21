7b3a7dc71f5ab9ad4603efd7e72927046ceaea62
hiroki-eguchi-lvgs <hiroki.eguchi@leverages.jp>
2026-08-21 17:33:08 +0900

    7.2. PrismaClientをPrismaServiceとしてラップし、NestJSのDIで注入できるようにする

--- eguchi-hiroki/blog-backend/src/app.module.ts
+++ eguchi-hiroki/blog-backend/src/app.module.ts
@@ -3,12 +3,14 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ArticlesModule } from './articles/articles.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: '.env.local',
     }),
+    PrismaModule,
     ArticlesModule,
   ],
   controllers: [AppController],
